a193f9244bd76a2f63b65af9fe57a895b72fb2fe
pinkjagnie <olga.krzysztofik@gmail.com>
2023-09-12 13:20:28 +0200

    Add title on register page

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -3,6 +3,7 @@ import RegisterForm from "../components/register/RegisterForm";
 const Page = () => {
   return (
     <section className="w-full min-h-screen pt-20 bg-slate-50">
+      <h1 className="py-4 text-xl text-center font-bold">Create an account</h1>
       <RegisterForm />
     </section>
   );
