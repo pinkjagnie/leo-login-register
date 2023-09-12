858e25ff2bec76e17c93e088cd992f7b7521852c
pinkjagnie <olga.krzysztofik@gmail.com>
2023-09-12 23:25:02 +0200

    Setup protected page

---
+++ src/app/protected/page.js
@@ -0,0 +1,11 @@
+const Page = () => {
+  return (
+    <section className="w-full min-h-screen pt-20 bg-slate-50">
+      <h1 className="py-4 text-xl text-center font-bold">
+        This is a protected page
+      </h1>
+    </section>
+  );
+};
+
+export default Page;
