b76f4a16068e8f4f45354e1e49948c5fa07e641a
pinkjagnie <olga.krzysztofik@gmail.com>
2023-09-13 00:07:47 +0200

    Add not fount page

---
+++ src/app/not-found.js
@@ -0,0 +1,26 @@
+"use client";
+
+import Link from "next/link";
+
+const NotFound = () => {
+  return (
+    <section className="flex items-center min-h-[100vh] p-16 pt-20">
+      <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
+        <div className="max-w-md text-center">
+          <h2 className="mb-8 font-extrabold text-7xl">Page not found</h2>
+          <p className="text-2xl font-semibold md:text-3xl">
+            Ups, something went wrong!
+          </p>
+          <Link
+            href="/"
+            className="mt-10 btn btn-active btn-accent hover:bg-slate-50"
+          >
+            Back to homepage
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default NotFound;
