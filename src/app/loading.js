d0fd30140f8fa1d7d97ab2a788fdc9a18ff1bd8e
pinkjagnie <olga.krzysztofik@gmail.com>
2023-09-13 00:13:39 +0200

    Improve general loading page

--- src/app/loading.js
+++ src/app/loading.js
@@ -1,17 +1,19 @@
 const Loading = () => {
   return (
-    <section className="min-h-[100vh]">
-      <div class="flex items-center justify-center min-h-screen p-5 bg-gray-100 min-w-screen">
+    <section className="min-h-[100vh] bg-slate-50 pt-64">
+      <div class="flex items-center justify-center p-5">
         <div class="flex space-x-2 animate-pulse">
-          <div class="w-3 h-3 bg-gray-500 rounded-full"></div>
-          <div class="w-3 h-3 bg-gray-500 rounded-full"></div>
-          <div class="w-3 h-3 bg-gray-500 rounded-full"></div>
+          <div class="w-4 h-4 bg-gray-500 rounded-full"></div>
+          <div class="w-4 h-4 bg-gray-500 rounded-full"></div>
+          <div class="w-4 h-4 bg-gray-500 rounded-full"></div>
         </div>
       </div>
-      <h1 className="mt-6 text-center text-2xl font-semibold md:text-3xl">
+      <h1 className="pt-6 text-center text-2xl font-semibold md:text-3xl">
         Loading...
       </h1>
-      <h2 className="mb-10 text-center text-xl font-semibold">please wait</h2>
+      <h2 className="pt-4 pb-10 text-center text-xl font-semibold">
+        please wait
+      </h2>
     </section>
   );
 };
