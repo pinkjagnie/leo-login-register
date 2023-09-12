4bd24ff31542db3f87fa9c0430474ff16fa26d0b
pinkjagnie <olga.krzysztofik@gmail.com>
2023-09-12 14:59:03 +0200

    Start implementing Formik with yup

--- src/app/components/register/RegisterForm.js
+++ src/app/components/register/RegisterForm.js
@@ -1,7 +1,16 @@
 "use client";
 import { useState } from "react";
+import { useFormik } from "formik";
 
 import { AiOutlineEyeInvisible, AiOutlineEye } from "react-icons/ai";
+
+import { schema } from "../../../schemas/index";
+
+const onSubmit = (values, actions) => {
+  console.log(values);
+
+  actions.resetForm();
+};
 
 const RegisterForm = () => {
   // visibility of password
@@ -21,8 +30,31 @@ const RegisterForm = () => {
     );
   // end of stuff about visibility of password
 
+  const {
+    values,
+    errors,
+    touched,
+    isSubmitting,
+    handleBlur,
+    handleChange,
+    handleSubmit,
+  } = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+      confirmPassword: "",
+      acceptedTOS: false,
+    },
+    validationSchema: schema,
+    onSubmit,
+  });
+
   return (
-    <form className="form-control w-[90%] md:w-[60%] p-6 mx-auto">
+    <form
+      className="form-control w-[90%] md:w-[60%] p-6 mx-auto"
+      autoComplete="off"
+      onSubmit={handleSubmit}
+    >
       <div className="pb-6">
         <label htmlFor="email" className="label">
           Email
@@ -30,8 +62,18 @@ const RegisterForm = () => {
         <input
           type="email"
           id="email"
-          className="input-ghost w-[100%] bg-slate-50 border-b border-gray-900"
+          value={values.email}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          className={`input-ghost w-[100%] bg-slate-50 border-b ${
+            errors.email && touched.email
+              ? "border-rose-500"
+              : "border-gray-900"
+          }`}
         />
+        {errors.email && touched.email && (
+          <p className="pt-2 text-sm text-rose-600">{errors.email}</p>
+        )}
       </div>
       <div className="pb-6">
         <label htmlFor="password" className="label">
@@ -41,7 +83,14 @@ const RegisterForm = () => {
           <input
             type={passwordType}
             id="password"
-            className="input-ghost w-[100%] bg-slate-50 border-b border-gray-900"
+            value={values.password}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            className={`input-ghost w-[100%] bg-slate-50 border-b ${
+              errors.password && touched.password
+                ? "border-rose-500"
+                : "border-gray-900"
+            }`}
           />
           <span
             className="-ml-8 my-auto cursor-pointer"
@@ -50,19 +99,75 @@ const RegisterForm = () => {
             {eyeIcon}
           </span>
         </div>
+        <p className="w-[95%] mx-auto pt-2 text-xs italic">
+          Password must be at least 8 characters long, contain one capital
+          letter, one number and one special character
+        </p>
+        {errors.password && touched.password && (
+          <p className="pt-2 text-sm text-rose-600">{errors.password}</p>
+        )}
+      </div>
+      <div className="pb-6">
+        <label htmlFor="confirmPassword" className="label">
+          Confirm password
+        </label>
+        <div className="flex">
+          <input
+            type={passwordType}
+            id="confirmPassword"
+            value={values.confirmPassword}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            className={`input-ghost w-[100%] bg-slate-50 border-b ${
+              errors.confirmPassword && touched.confirmPassword
+                ? "border-rose-500"
+                : "border-gray-900"
+            }`}
+          />
+          <span
+            className="-ml-8 my-auto cursor-pointer"
+            onClick={toggleShowPassword}
+          >
+            {eyeIcon}
+          </span>
+        </div>
+        <p className="w-[95%] mx-auto pt-2 text-xs italic">
+          Password must be at least 8 characters long, contain one capital
+          letter, one number and one special character
+        </p>
+        {errors.confirmPassword && touched.confirmPassword && (
+          <p className="pt-2 text-sm text-rose-600">{errors.confirmPassword}</p>
+        )}
       </div>
       <div className="pb-6 flex items-center justify-start">
-        <input type="checkbox" id="selectCheckbox" className="checkbox" />
-        <label
-          htmlFor="selectCheckbox"
-          className="label-text cursor-pointer pl-2"
-        >
+        <input
+          type="checkbox"
+          id="acceptedTOS"
+          name="accteptedTOS"
+          value={values.acceptedTOS}
+          className="checkbox"
+        />
+        <label htmlFor="acceptedTOS" className="label-text cursor-pointer pl-2">
           I accept the terms of service
         </label>
       </div>
-      <button type="submit" className="btn btn-outline">
-        Create an account
-      </button>
+      {errors.acceptedTOS && touched.acceptedTOS && (
+        <p className="-mt-4 pb-6 text-sm text-rose-600">{errors.acceptedTOS}</p>
+      )}
+      {!isSubmitting ? (
+        <button
+          disabled={isSubmitting}
+          type="submit"
+          className="btn btn-outline"
+        >
+          Create an account
+        </button>
+      ) : (
+        <button className="btn">
+          <span className="loading loading-spinner"></span>
+          Submitting
+        </button>
+      )}
     </form>
   );
 };
