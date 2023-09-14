37352149859011d960506dde31d1c87b7ce59006
pinkjagnie <olga.krzysztofik@gmail.com>
2023-09-14 20:46:58 +0200

    Connect & adding new tornado to pb

--- src/app/components/tornadoes/AddTornadoForm.js
+++ src/app/components/tornadoes/AddTornadoForm.js
@@ -1,52 +1,88 @@
 "use client";
 
+import { useState } from "react";
 import { Form, Formik } from "formik";
 
+import pb from "@/lib/pocketbase";
 import { tornadoSchema } from "@/schemas/index";
 
 import CustomInput from "../form/custom/CustomInput";
 import CustomTextarea from "../form/custom/CustomTextarea";
+import Message from "../form/Message";
 
 const AddTornadoForm = () => {
+  const [message, setMessage] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
+
+  const onSubmit = async (values, actions) => {
+    console.log(values);
+
+    const enteredTitle = values.Title;
+    const enteredShortMsg = values.ShortMessage;
+    const enteredMsg = values.Message;
+
+    const data = {
+      UserID: 1234,
+      Title: enteredTitle,
+      ShortMessage: enteredShortMsg,
+      Message: enteredMsg,
+    };
+
+    try {
+      const record = await pb.collection("tornadoes").create(data);
+
+      console.log("record ", record);
+      actions.resetForm();
+
+      setMessage("Tornado successfully created!");
+    } catch (error) {
+      console.log(error);
+      setErrorMsg("Something went wrong! " + error.data.message);
+    }
+  };
+
   return (
-    <Formik
-      initialValues={{
-        Title: "",
-        ShortMessage: "",
-        Message: "",
-        // Attachment: ""
-      }}
-      validationSchema={tornadoSchema}
-      onSubmit={(values, actions) => {
-        console.log(values);
-      }}
-    >
-      {({ isSubmitting }) => (
-        <Form className="form-control w-[90%] md:w-[60%] lg:w-[50%] p-6 mx-auto mt-2 mb-14 border-2 border-stone-200 rounded-md bg-stone-200">
-          <CustomInput label="Title *" name="Title" type="text" />
-          <CustomInput
-            label="Short message *"
-            name="ShortMessage"
-            type="text"
-          />
-          <CustomTextarea label="Message *" name="Message" type="text" />
-          {!isSubmitting ? (
-            <button
-              disabled={isSubmitting}
-              type="submit"
-              className="btn btn-outline bg-zinc-400 w-[80%] mx-auto block"
-            >
-              Add new tornado
-            </button>
-          ) : (
-            <button className="btn btn-active btn-accent text-slate-50 w-[80%] mx-auto block">
-              <span className="loading loading-spinner text-slate-50"></span>
-              Adding
-            </button>
-          )}
-        </Form>
+    <>
+      {(message || errorMsg) && (
+        <Message message={message} errorMsg={errorMsg} />
       )}
-    </Formik>
+      <Formik
+        initialValues={{
+          Title: "",
+          ShortMessage: "",
+          Message: "",
+          // Attachment: ""
+        }}
+        validationSchema={tornadoSchema}
+        onSubmit={onSubmit}
+      >
+        {({ isSubmitting }) => (
+          <Form className="form-control w-[90%] md:w-[60%] lg:w-[50%] p-6 mx-auto mt-2 mb-14 border-2 border-stone-200 rounded-md bg-stone-200">
+            <CustomInput label="Title *" name="Title" type="text" />
+            <CustomInput
+              label="Short message *"
+              name="ShortMessage"
+              type="text"
+            />
+            <CustomTextarea label="Message *" name="Message" type="text" />
+            {!isSubmitting ? (
+              <button
+                disabled={isSubmitting}
+                type="submit"
+                className="btn btn-outline bg-zinc-400 w-[80%] mx-auto block"
+              >
+                Add new tornado
+              </button>
+            ) : (
+              <button className="btn btn-active btn-accent text-slate-50 w-[80%] mx-auto block">
+                <span className="loading loading-spinner text-slate-50"></span>
+                Adding
+              </button>
+            )}
+          </Form>
+        )}
+      </Formik>
+    </>
   );
 };
 
