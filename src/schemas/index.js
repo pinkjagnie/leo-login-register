149ee9482e5d8a9a074637220981c44bb44d70fa
pinkjagnie <olga.krzysztofik@gmail.com>
2023-09-14 20:24:59 +0200

    Add schema for validation new tornado

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -1,5 +1,7 @@
 import * as yup from "yup";
 
+// AUTH SCHEMAS
+
 const passwordRules = /^(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9]).{8,}$/;
 // regex for password - min 8 characters, 1 capital letter, 1 number, 1 special character
 
@@ -38,4 +40,47 @@ export const loginSchema = yup.object().shape({
         "Password must be at least 8 characters long, contain one capital letter, one number and one special character",
     })
     .required("This field is required"),
+});
+
+// TORNADO SCHEMA
+
+const MAX_FILE_SIZE = 102400; //100KB
+
+const validFileExtensions = {
+  image: ["jpg", "gif", "png", "jpeg", "svg", "webp"],
+};
+
+function isValidFileType(fileName, fileType) {
+  return (
+    fileName &&
+    validFileExtensions[fileType].indexOf(fileName.split(".").pop()) > -1
+  );
+}
+
+export const tornadoSchema = yup.object().shape({
+  Title: yup
+    .string()
+    .min(2, "Minimum length: 2 characters")
+    .max(20, "Maximum length: 20 characters")
+    .required("This field is required"),
+  ShortMessage: yup
+    .string()
+    .min(2, "Minimum length: 2 characters")
+    .max(40, "Maximum length: 40 characters")
+    .required("This field is required"),
+  Message: yup
+    .string()
+    .min(2, "Minimum length: 2 characters")
+    .max(200, "Maximum length: 200 characters")
+    .required("This field is required"),
+  // Attachment: yup
+  //   .mixed()
+  //   .test("is-valid-type", "Not a valid image type", (value) =>
+  //     isValidFileType(value && value.name.toLowerCase(), "image")
+  //   )
+  //   .test(
+  //     "is-valid-size",
+  //     "Max allowed size is 100KB",
+  //     (value) => value && value.size <= MAX_FILE_SIZE
+  //   ),
 });
