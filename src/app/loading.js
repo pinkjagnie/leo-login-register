4d4a5ec3fff45075f6d6517180005e294f94cc66
pinkjagnie <olga.krzysztofik@gmail.com>
2023-09-14 13:32:49 +0200

    Fixed typos in loading page

--- src/app/loading.js
+++ src/app/loading.js
@@ -1,11 +1,11 @@
 const Loading = () => {
   return (
     <section className="min-h-[100vh] bg-slate-50 pt-64">
-      <div class="flex items-center justify-center p-5">
-        <div class="flex space-x-2 animate-pulse">
-          <div class="w-4 h-4 bg-gray-500 rounded-full"></div>
-          <div class="w-4 h-4 bg-gray-500 rounded-full"></div>
-          <div class="w-4 h-4 bg-gray-500 rounded-full"></div>
+      <div className="flex items-center justify-center p-5">
+        <div className="flex space-x-2 animate-pulse">
+          <div className="w-4 h-4 bg-gray-500 rounded-full"></div>
+          <div className="w-4 h-4 bg-gray-500 rounded-full"></div>
+          <div className="w-4 h-4 bg-gray-500 rounded-full"></div>
         </div>
       </div>
       <h1 className="pt-6 text-center text-2xl font-semibold md:text-3xl">
