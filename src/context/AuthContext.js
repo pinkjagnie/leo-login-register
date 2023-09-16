25cf509ffcbdac3994e56b5594bf6d30b230a544
pinkjagnie <olga.krzysztofik@gmail.com>
2023-09-16 12:38:46 +0200

    Rebuild AutContext

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,15 +1,70 @@
-import { createContext, useContext, useState } from "react";
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
+import { useInterval } from "usehooks-ts";
+import PocketBase from "pocketbase";
+import jwtDecode from "jwt-decode";
+import ms from "ms";
+
+const fiveMinutesInMs = ms("5 minutes");
+const twoMinutesInMs = ms("2 minutes");
 
 const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [userID, setUserID] = useState("");
+  const pb = useMemo(() => new PocketBase(process.env.NEXT_APP_PB_URL), []);
+
+  const [token, setToken] = useState(pb.authStore.token);
+  const [user, setUser] = useState(pb.authStore.model); // user gonna be -> all infos from pb about currently logged in user (e.g.: UserIdentificator, email)
+
+  useEffect(() => {
+    return pb.authStore.onChange((token, model) => {
+      setToken(token);
+      setUser(model);
+    });
+  }, []);
+
+  useEffect(() => {
+    console.log(token);
+    console.log(user);
+  }, [token, user]);
+
+  const register = useCallback(
+    async (email, password, passwordConfirm, UserIdentificator) => {
+      return await pb
+        .collection("users")
+        .create({ email, password, passwordConfirm, UserIdentificator });
+    },
+    []
+  );
+
+  const login = useCallback(async (email, password) => {
+    return await pb.collection("users").authWithPassword(email, password);
+  }, []);
+
+  const logout = useCallback(() => {
+    pb.authStore.clear();
+  }, []);
+
+  const refreshSession = useCallback(async () => {
+    if (!pb.authStore.isValid) return;
+    const decoded = jwtDecode(token);
+    const tokenExpiration = decoded.exp;
+    const expirationWithBuffer = (decoded.exp + fiveMinutesInMs) / 1000;
+    if (tokenExpiration < expirationWithBuffer) {
+      await pb.collection("users").authRefresh();
+    }
+  }, [token]);
+
+  useInterval(refreshSession, token ? twoMinutesInMs : null);
 
   return (
-    <AuthContext.Provider
-      value={{ isLoggedIn, setIsLoggedIn, userID, setUserID }}
-    >
+    <AuthContext.Provider value={{ register, login, logout, user, token, pb }}>
       {children}
     </AuthContext.Provider>
   );
