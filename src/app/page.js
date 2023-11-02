2f4a45995d35ee3b4427d87181680b4d6b40a0e3
pinkjagnie <olga.krzysztofik@gmail.com>
2023-11-02 16:39:32 +0100

    Changed styling of main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,16 @@
+import { WiTornado } from "react-icons/wi";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-slate-50">
-      <p>Hi</p>
+    <main className="flex min-h-screen flex-col items-center justify-between py-24 px-0 bg-slate-50">
+      <div className="w-[90%] md:w-[70%] lg:w-[60%] mx-auto my-auto md:mt-16 text-center font-bold text-5xl">
+        <h1>Welcome to the tornado side of your mind</h1>
+        <h1>where you have the opportunity to share your thoughts</h1>
+      </div>
+      <WiTornado
+        size={500}
+        className="hidden md:visible md:block md:absolute right-4 bottom-4 opacity-20 lg:h-[350px] lg:w-[auto]"
+      />
     </main>
   );
 }
